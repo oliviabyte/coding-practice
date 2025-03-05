edc669eaa4625fb0e87fd74a49ba5c7d278efc48
Tanya <2093336837@qq.com>
2025-03-05 01:47:42 -0800

    add leetcode0560

---
+++ leetcode/0560.Subarray Sum Equals K.java
@@ -0,0 +1,21 @@
+package leetcode;
+import java.util.HashMap;
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int[] s = new int[nums.length+1];
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            s[i+1] = s[i] + nums[i];
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int sj : s) {
+            if(map.containsKey(sj - k)) {
+                count += map.get(sj - k);
+            }
+            map.put(sj, map.getOrDefault(sj, 0)+1);
+        }
+        return count;
+    }
+}
