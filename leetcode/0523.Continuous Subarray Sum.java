6932e1805914adf1ef0a5fbc1f447b4a8df0a9d4
Tanya <2093336837@qq.com>
2025-03-05 16:58:01 -0800

    add leetcode0523

---
+++ leetcode/0523.Continuous Subarray Sum.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.HashMap;
+
+// s[i+1] = s[i]+nums[i]
+// (s[i+1] - s[j]) % k = 0 -> s[i+1] % k = s[j] % k
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        // 1 create the prefix array
+        int[] s = new int[nums.length+1];
+        for(int i = 0; i < nums.length; i++) {
+            s[i+1] = s[i] + nums[i];
+        }
+        // 2 create the hashmap, check if there's a same remainder in the hashmap
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int j = 0; j < s.length; j++) {
+            int remainder = s[j] % k;
+            if(map.containsKey(remainder)) {
+                if(j - map.get(remainder) >= 2) return true;
+            } else {
+                // 3 put(remainder, j)
+                map.put(remainder, j);
+            }
+                
+        }
+        return false;
+    }
+}
+// time: O(N)
+// space: O(N)
