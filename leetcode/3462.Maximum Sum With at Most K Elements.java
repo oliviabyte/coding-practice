37dc62a48f8ba2f70709207653b7794fb48bfdd5
Tanya <2093336837@qq.com>
2025-03-08 21:34:23 -0800

    add leetcode3462

---
+++ leetcode/3462.Maximum Sum With at Most K Elements.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+class Solution {
+    public long maxSum(int[][] grid, int[] limits, int k) {
+        long res = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for(int i = 0; i < grid.length; i++) {
+            Arrays.sort(grid[i]);
+            
+            for(int j = grid[i].length - 1; j >= 0; j--) {
+                if(limits[i] != 0) {
+                pq.add(grid[i][j]);
+                limits[i]--;
+                }
+            }
+        }
+        while(k > 0 && !pq.isEmpty()) {
+            res += pq.poll();
+            k--;
+        }
+        return res;
+    }
+}
