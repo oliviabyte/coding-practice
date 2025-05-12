545ab156b25cf41b4a527af1325c083df38b5d2c
Tanya <2093336837@qq.com>
2025-05-11 18:29:53 -0700

    add leetcode503

---
+++ leetcode/0503. Next Greater Element II.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.Stack;
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 2 * n - 1; i >= 0; i--) {
+            int idx = i % n;
+            while(!stack.isEmpty() && nums[idx] >= stack.peek()) stack.pop();
+            res[idx] = stack.isEmpty()? -1:stack.peek();
+            stack.push(nums[idx]);
+        }
+        return res;
+    }
+}
