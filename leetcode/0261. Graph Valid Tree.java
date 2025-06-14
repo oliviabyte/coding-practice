3177c4bbf14eb4c8f6958df66a1b760424dc343d
Tanya <2093336837@qq.com>
2025-06-13 21:51:18 -0700

    6.13

---
+++ leetcode/0261. Graph Valid Tree.java
@@ -0,0 +1,38 @@
+package leetcode;
+import java.util.List;
+import java.util.ArrayList;
+
+
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        // 判断有没有环（如果是树edges数量必须是节点数量-1）
+        if (edges.length != n - 1) return false;
+
+        List<Integer>[] graph = new ArrayList[n];
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        // build the graph
+        for(int[] edge : edges) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+        dfs(0, graph, visited);
+
+        // 判断连通性（是不是一整块连通分量）
+        for(boolean visit : visited) {
+            if(!visit) return false;
+        }
+        return true;
+        
+    }
+    private void dfs(int curr, List<Integer>[] graph, boolean[] visited) {
+        visited[curr] = true;
+        for(int neighbor : graph[curr]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, graph, visited);
+            }
+        }
+    }
+}
