c3bb0faea92af5ebad01d42345e9f2b99741ef11
Tanya <2093336837@qq.com>
2025-07-21 23:12:16 -0700

    7.21

---
+++ leetcode/0636. Exclusive Time of Functions.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.List;
+import java.util.Stack;
+
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        Stack<int[]> stack = new Stack<>();
+        int[] result = new int[n];
+        int lastestTime = 0;
+        for(String log : logs) {
+            String[] part = log.split(":");
+            int currNum = Integer.parseInt(part[0]);
+
+            if(part[1].equals("start")) {
+                int currStart = Integer.parseInt(part[2]);
+                if(stack.isEmpty()) {
+                    stack.push(new int[] {currNum, currStart});
+                    lastestTime = currStart;
+                    continue;
+                }
+                result[stack.peek()[0]] += currStart - lastestTime;
+                stack.push(new int[] {currNum, currStart});
+                lastestTime = currStart;
+
+            } else {
+                int currEnd = Integer.parseInt(part[2]);
+                result[currNum] += currEnd - lastestTime + 1;
+                lastestTime = currEnd + 1;
+                stack.pop();
+            }
+        }
+        return result;
+    }
+}
