bd9cc3cd0040537076aef0aab910898a5af83ae4
Tanya <2093336837@qq.com>
2025-07-22 23:10:31 -0700

    7.22

---
+++ leetcode/0173. Binary Search Tree Iterator.java
@@ -0,0 +1,69 @@
+package leetcode;
+
+import java.util.Stack;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+            TreeNode() {}
+            TreeNode(int val) { this.val = val; }
+            TreeNode(int val, TreeNode left, TreeNode right) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        Stack<TreeNode> stack;
+        TreeNode curr;
+    class BSTIterator {
+        public BSTIterator(TreeNode root) {
+            stack = new Stack<>();
+            curr = root;
+            while(curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+                }
+        }
+        public int next() {
+            TreeNode node = stack.pop();
+                if(node.right != null) {
+                    TreeNode p = node.right;
+                    while(p != null) {
+                        stack.push(p);
+                        p = p.left;
+                    }
+                }
+                return node.val;
+        }
+        
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+    }
+}
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
