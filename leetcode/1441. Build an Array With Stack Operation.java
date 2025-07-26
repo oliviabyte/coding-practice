c10cdf740f40920dc16bf1676e062a930849f299
Tanya <2093336837@qq.com>
2025-07-25 18:12:52 -0700

    7.25

---
+++ leetcode/1441. Build an Array With Stack Operation.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        int pointer = 0;
+        List<String> res = new ArrayList<>();
+        for(int i = 1; i <= n; i++) {
+            if(pointer == target.length) return res;
+            res.add("Push");
+            if(i != target[pointer]) res.add("Pop");
+            else pointer++;
+        }
+        return res;
+
+    }
+}
